3ffdb23fea1ad53d08e6c58e37f4ada899950a8e
MarselDokubo <connectmarsel@gmail.com>
2025-02-12 21:08:16 +0100

    fix(ui): mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Pricing from "./_components/pricing";
 
 export default function Home() {
   return (
-    <>
+    <div className="overflow-x-hidden">
       <NavBar />
       <section className=" w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center justify-center  antialiased">
         <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
@@ -18,6 +18,6 @@ export default function Home() {
         <HeroParallax products={products} />
       </section>
       <Pricing />
-    </>
+    </div>
   );
 }
