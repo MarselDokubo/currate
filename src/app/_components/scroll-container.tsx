742b52f4ec05f1e14c6f44fa754a396b9139bd36
MarselDokubo <connectmarsel@gmail.com>
2025-02-12 17:02:42 +0100

    feat(auth): Clerk Authentication

---
+++ src/app/_components/scroll-container.tsx
@@ -0,0 +1,96 @@
+"use client";
+import Image from "next/image";
+import { Button } from "~/components/ui/button";
+import { useEffect, useRef, useState } from "react";
+import { motion } from "motion/react";
+import { useScroll, useTransform } from "motion/react";
+import type { MotionValue } from "motion/react";
+export default function ScrollContainer() {
+  const containerRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+  });
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => {
+      window.removeEventListener("resize", checkMobile);
+    };
+  }, []);
+  const scaleDimensions = () => {
+    return isMobile ? [0.7, 0.9] : [1.05, 1];
+  };
+  const rotate = useTransform(scrollYProgress, [0, 1], [20, 0]);
+  const scale = useTransform(scrollYProgress, [0, 1], scaleDimensions());
+  //const translate = useTransform(scrollYProgress, [0, 1], [0, -100]);
+  return (
+    <div
+      className="flex items-center justify-center relative p-20 mt-20"
+      ref={containerRef}
+    >
+      <div
+        className="w-full relative"
+        style={{
+          perspective: "1000px",
+        }}
+      >
+        <HeroText />
+        <HeroImage rotate={rotate} scale={scale} />
+      </div>
+    </div>
+  );
+}
+
+export function HeroText() {
+  return (
+    <motion.div className="flex flex-col">
+      <div className="flex items-center flex-col">
+        <Button
+          size={"lg"}
+          className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+        >
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
+            Start For Free Today
+          </span>
+        </Button>
+        <h1 className="text-5xl md:text-8xl bg-clip-text tracking-tighter text-transparent bg-gradient-to-b from-white to-neutral-600 font-semibold">
+          Automate with Currate
+        </h1>
+      </div>
+    </motion.div>
+  );
+}
+
+export function HeroImage({
+  rotate,
+  scale,
+}: {
+  rotate: MotionValue<number>;
+  scale: MotionValue<number>;
+}) {
+  return (
+    <motion.div
+      className="max-w-5xl mx-auto w-full rounded-[30px] shadow-2xl"
+      style={{
+        rotateX: rotate,
+        scale,
+        boxShadow:
+          "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
+      }}
+    >
+      <div className="bg-neutral-900 rounded-2xl  gap-4 overflow-hidden transition-all flex items-center justify-center">
+        <Image
+          src="/temp-banner.png"
+          alt="bannerImage"
+          width={3456}
+          height={2064}
+          className="object-cover border-8 border-neutral-900 rounded-2xl w-full h-full"
+        />
+      </div>
+    </motion.div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import NavBar from "./_components/navbar";
+import ScrollContainer from "./_components/scroll-container";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="font-semibold">Currate</h1>
-    </div>
+    <>
+      <NavBar />
+      <section className="min-h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center justify-center  antialiased">
+        <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+        <ScrollContainer />
+      </section>
+    </>
   );
 }
