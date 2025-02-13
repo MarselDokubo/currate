10e9a54516feed55b785c33fda0517a87a5ab345
MarselDokubo <connectmarsel@gmail.com>
2025-02-13 13:08:45 +0100

    feat(ui): nested layout navigation

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 //const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/forum(.*)"]);
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+//const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
+const isProtectedRoute = createRouteMatcher([""]);
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) await auth.protect();
+  if (isProtectedRoute(req)) await auth.protect();
 });
 export const config = {
   matcher: [
